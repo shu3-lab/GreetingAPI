b9faa7f5bd19f2b4a276236818f150fd4b1de723
shu3-lab <shushushu912@gmail.com>
2020-12-06 14:10:39 +0900

    Add unit tests of domain

---
+++ src/test/java/domain/GreetingDomainTest.java
@@ -0,0 +1,60 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.myapi.domain.GreetingDomain;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class GreetingDomainTest {
+    
+    private static GreetingDomain domain;
+
+    @BeforeAll
+    public static void setUp() {
+        domain = new GreetingDomain();
+    }
+
+    @Test
+    public void caseHello(){
+        String actual = domain.responseGreeting("hello");
+        String expected = "Hello, everyone!";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void caseMorning(){
+        String actual = domain.responseGreeting("morning");
+        String expected = "Good morning, everyone!";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void caseEvening(){
+        String actual = domain.responseGreeting("evening");
+        String expected = "Good evening, everyone!";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void caseNumber(){
+        String actual = domain.responseGreeting("1");
+        String expected = "Missing your choice...";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void caseBrank(){
+        String actual = domain.responseGreeting("");
+        String expected = "Missing your choice...";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void caseSpace(){
+        String actual = domain.responseGreeting(" ");
+        String expected = "Missing your choice...";
+        assertEquals(expected, actual);
+    }
+}
